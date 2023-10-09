2bd219c59acb834ae842da97133d8ba6a09837b1
xhmueel <samuelbrazpinto@tecnico.ulisboa.pt>
2023-10-09 16:25:40 +0100

    feat: implement fake inventory

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static ItemMenu;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    private List<ItemMenu> _items;
+    public GameObject StickyNotePrefab;
+    private GameObject _inventoryRegion;
+
+    public static InventoryManager Instance { get; set;}
+
+
+    public void Start()
+    {
+        Instance = this;
+
+        _inventoryRegion = GameObject.Find("Item1Region");
+
+        _items = new List<ItemMenu>()
+        {
+            new ItemMenu("Stone", 3, Instantiate(StickyNotePrefab), _inventoryRegion)
+        };
+
+        ReloadItems();
+    }
+
+    public void Update()
+    {
+        ReloadItems();
+    }
+
+
+    private void ReloadItems()
+    {
+        foreach(ItemMenu item in _items)
+        {
+            item.StickyNote.transform.SetParent(_inventoryRegion.transform, false);
+        }
+    }
+
+
+
+}
