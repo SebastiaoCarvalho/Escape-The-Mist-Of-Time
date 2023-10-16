4a1a9f0d95e9820f30f00d761a9b2e3d01b82741
xhmueel <samuelbrazpinto@tecnico.ulisboa.pt>
2023-10-16 15:58:15 +0100

    feat: Create Game Data Scriptable Object

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -14,7 +14,7 @@ public class MenuManager : MonoBehaviour
     {
         //In a "real game" we would want GameData to be persistent, but for testing, we need to reset it
         GameData.CleanData();
-
+        
         GameData.toDoTasks = new List<Task>(){
             new Task("Task D", "survive you dumbass", Instantiate(TaskManager.Instance.StickyNotePrefab), TaskManager.Instance._toDoRegion.name),
             new Task("Task C", "Just die please", Instantiate(TaskManager.Instance.StickyNotePrefab), TaskManager.Instance._toDoRegion.name)
@@ -28,13 +28,13 @@ public class MenuManager : MonoBehaviour
 
         UpgradeManager.Instance._head = new Upgrade("Speed boost", "Boost your speed", 0, Instantiate(UpgradeManager.Instance.UpgradePrefab));
         GameData.upgrades = new List<Upgrade>()
-        {
+    {
             UpgradeManager.Instance._head,
             new Upgrade("Mind kill", "Boost your psychic powers", 1, UpgradeManager.Instance._head, Instantiate(UpgradeManager.Instance.UpgradePrefab)),
             new Upgrade("Flying", "Spread your wings", 1, UpgradeManager.Instance._head, Instantiate(UpgradeManager.Instance.UpgradePrefab))
         };
 
-
+        
     }
 
     
