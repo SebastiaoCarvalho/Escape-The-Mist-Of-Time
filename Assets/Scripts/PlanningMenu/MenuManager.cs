51d6e3707fe2271c0c375f02daa473f8d16d44b6
SebastiaoCarvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2023-10-18 00:46:29 +0100

    small restructuration to code

--- Assets/Scripts/PlanningMenu/MenuManager.cs
+++ Assets/Scripts/PlanningMenu/MenuManager.cs
@@ -12,31 +12,37 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GameData.DebugData();
         //In a "real game" we would want GameData to be persistent, but for testing, we need to reset it
-        GameData.CleanData();
-        
-        GameData.toDoTasks = new List<Task>(){
-            new Task("Task D", "survive you dumbass", Instantiate(TaskManager.Instance.StickyNotePrefab), TaskManager.Instance._toDoRegion.name),
-            new Task("Task C", "Just die please", Instantiate(TaskManager.Instance.StickyNotePrefab), TaskManager.Instance._toDoRegion.name)
+        if (GameData.IsClean)
+        {
+            InitializeGameData();
+        }
+    }
+
+    private void InitializeGameData() {
+        GameData.ToDoTasks = new List<Task>(){
+            new Task("Task D", "survive you dumbass", TaskManager.Instance._toDoRegion.name),
+            new Task("Task C", "Just die please", TaskManager.Instance._toDoRegion.name)
         };
-        GameData.inProgressTasks = new List<Task>() {
-            new Task("Task B", "kill 'Mr Capeta'", Instantiate(TaskManager.Instance.StickyNotePrefab), TaskManager.Instance._inProgressRegion.name)
+        GameData.InProgressTasks = new List<Task>() {
+            new Task("Task B", "kill 'Mr Capeta'", TaskManager.Instance._inProgressRegion.name)
         };
-        GameData.completedTasks = new List<Task>() {
-            new Task("Task A", "simply exist", Instantiate(TaskManager.Instance.StickyNotePrefab), TaskManager.Instance._completedRegion.name)
+        GameData.CompletedTasks = new List<Task>() {
+            new Task("Task A", "simply exist", TaskManager.Instance._completedRegion.name)
         };
-    
 
         UpgradeManager.Instance._head = new Upgrade("Speed boost", "Boost your speed", 0, Instantiate(UpgradeManager.Instance.UpgradePrefab));
-        GameData.upgrades = new List<Upgrade>()
+        GameData.Upgrades = new List<Upgrade>()
         {
             UpgradeManager.Instance._head,
             new Upgrade("Mind kill", "Boost your psychic powers", 1, UpgradeManager.Instance._head, Instantiate(UpgradeManager.Instance.UpgradePrefab)),
             new Upgrade("Flying", "Spread your wings", 1, UpgradeManager.Instance._head, Instantiate(UpgradeManager.Instance.UpgradePrefab))
         };
-
-
     }
 
-    
+    public void Clean() {
+        GameData.CleanData();
+    }
+
 }
