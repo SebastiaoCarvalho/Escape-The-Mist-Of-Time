6f9a3cf2794da7fa3ea54f2e5b3d677f54b7702e
xhmueel <105432909+xhmueel@users.noreply.github.com>
2023-10-17 22:57:15 +0100

    feat: Implement Menu Scrolling in Main Menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public static MainMenuManager Instance { get; set; }
+    private Dictionary<string, GameObject> _buttons;
+    private ButtonEnum _currentButton = ButtonEnum.Unsigned;
+
+
+    private enum ButtonEnum
+    {
+        Start,
+        Quit,
+        Unsigned
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        _buttons = new Dictionary<string, GameObject>();
+
+        GameObject.FindGameObjectsWithTag("MainMenuButton").ToList().ForEach(button =>
+        {
+            _buttons.Add(button.name, button);
+        });
+    }
+
+    public void LoadPlanningMenu()
+    {
+        SceneManager.LoadSceneAsync("PlanningMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            if(_currentButton == ButtonEnum.Unsigned)
+            {
+                _currentButton = ButtonEnum.Start;
+                EventSystem.current.SetSelectedGameObject(_buttons["Play"]);
+            }
+            else if(_currentButton == ButtonEnum.Start)
+            {
+                _currentButton = ButtonEnum.Quit;
+                EventSystem.current.SetSelectedGameObject(_buttons["Quit"]);
+            }
+            else if(_currentButton == ButtonEnum.Quit)
+            {
+                _currentButton = ButtonEnum.Start;
+                EventSystem.current.SetSelectedGameObject(_buttons["Play"]);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (_currentButton == ButtonEnum.Quit)
+            {
+                _currentButton = ButtonEnum.Start;
+                EventSystem.current.SetSelectedGameObject(_buttons["Play"]);
+            }
+            else if(_currentButton == ButtonEnum.Start)
+            {
+                _currentButton = ButtonEnum.Quit;
+                EventSystem.current.SetSelectedGameObject(_buttons["Quit"]);
+            }
+
+        }
+    }
+}
