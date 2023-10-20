ca57e6d38771b0a4cd8917c504a8ac2ae825870c
fallenatlas <tiago.a.p.antunes@gmail.com>
2023-10-20 12:14:29 +0100

    change enemy state transitions to account with player respawn

--- Assets/Scripts/Game/AI/EnemyTransitions/LostPlayer.cs
+++ Assets/Scripts/Game/AI/EnemyTransitions/LostPlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 namespace Escape.Game.AI.EnemyTransitions {
     public class LostPlayer : Transition {
 
+        private GameManager _gameManager;
         private GameObject _character;
         private GameObject _player;
         private float _sightDistance = 15f;
@@ -10,12 +11,13 @@ namespace Escape.Game.AI.EnemyTransitions {
         public LostPlayer(GameObject character, GameObject player) {
             _character = character;
             _player = player;
+            _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         }
 
         public override bool IsTriggered() {
             Vector2 characterPos = new Vector2(_character.transform.position.x, _character.transform.position.z);
             Vector2 playerPos = new Vector2(_player.transform.position.x, _player.transform.position.z);
-            return Vector3.Distance(characterPos, playerPos) > _sightDistance;
+            return _gameManager.timeOut || Vector3.Distance(characterPos, playerPos) > _sightDistance;
         }
     }
 }
--- Assets/Scripts/Game/AI/EnemyTransitions/SawPlayer.cs
+++ Assets/Scripts/Game/AI/EnemyTransitions/SawPlayer.cs
@@ -3,19 +3,23 @@ using UnityEngine;
 namespace Escape.Game.AI.EnemyTransitions {
     public class SawPlayer : Transition {
 
+        private GameManager _gameManager;
         private GameObject _character;
         private GameObject _player;
+        private Player _playerScript;
         private float _sightDistance = 10f;
 
         public SawPlayer(GameObject character, GameObject player) {
             _character = character;
             _player = player;
+            _playerScript = player.GetComponent<Player>();
+            _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         }
 
         public override bool IsTriggered() {
             Vector2 characterPos = new Vector2(_character.transform.position.x, _character.transform.position.z);
             Vector2 playerPos = new Vector2(_player.transform.position.x, _player.transform.position.z);
-            return Vector3.Distance(characterPos, playerPos) < _sightDistance;
+            return !_gameManager.timeOut && _playerScript.Moved && Vector3.Distance(characterPos, playerPos) < _sightDistance;
         }
     }
 }
